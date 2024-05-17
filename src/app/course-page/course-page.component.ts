8186af68e7a21aa6de64dc1e59942513cabfe080
Hanin Farhan <haninfarhan96@gmail.com>
2024-05-17 14:02:19 +0200

    Installerat fontawesome och lagt till component icon, minstat antal kurser på skärm för snabbare utveckling tillfälligt

--- src/app/course-page/course-page.component.ts
+++ src/app/course-page/course-page.component.ts
@@ -2,23 +2,31 @@ import { Component } from '@angular/core';
 import { Course } from '../model/course';
 import { CourseService } from '../services/course.service';
 import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-course-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FontAwesomeModule],
   templateUrl: './course-page.component.html',
   styleUrl: './course-page.component.css'
 })
 export class CoursePageComponent {
   //Initierar till tom array från interface
   courseData: Course[] = [];
+  faLink = faArrowUpRightFromSquare;
 
-  constructor(private courseservice : CourseService) {}
+  constructor(private courseservice: CourseService) { }
 
   ngOnInit() {
     this.courseservice.getCourseData().subscribe(data => {
       this.courseData = data;
+      this.courseData = this.courseData.slice(0, 9);
     })
   }
+
+  //Hero img
+  students1Img: string = "assets/img/CF017232.IIQ.p.jpg";
 }
